dd2ce8c141e1882022dc7012180b63ca88b26074
Zach Lefler <leaving.valhalla@gmail.com>
2022-02-26 18:12:03 -0800

    commit tree 1c1f40bafd05ea91813f0c4ab051861caac4988c

--- main.js
+++ main.js
@@ -1,24 +1,30 @@
 // Defining text characters for the empty and full hearts for you to use later.
-const EMPTY_HEART = '♡'
-const FULL_HEART = '♥'
+const EMPTY_HEART = '♡';
+const FULL_HEART = '♥';
 
 // Your JavaScript code goes here!
 
+const hearts = document.getElementsByClassName('like-glyph');
+console.log(hearts);
 
-
+hearts.forEach((heart) => {
+  heart.addEventListener('click', (e) => {
+    mimicServerCall();
+  });
+});
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
 
-function mimicServerCall(url="http://mimicServer.example.com", config={}) {
-  return new Promise(function(resolve, reject) {
-    setTimeout(function() {
-      let isRandomFailure = Math.random() < .2
+function mimicServerCall(url = 'http://mimicServer.example.com', config = {}) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      let isRandomFailure = Math.random() < 0.2;
       if (isRandomFailure) {
-        reject("Random server error. Try again.");
+        reject('Random server error. Try again.');
       } else {
-        resolve("Pretend remote server notified of action!");
+        resolve('Pretend remote server notified of action!');
       }
     }, 300);
   });
